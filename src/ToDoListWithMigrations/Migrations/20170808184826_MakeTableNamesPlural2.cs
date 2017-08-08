0e139a3694688555ef9f2221a5d67cabf183d216
david rolfs <davidrolfs15@gmail.com>
2017-08-08 12:23:52 -0700

    add model test

---
+++ src/ToDoListWithMigrations/Migrations/20170808184826_MakeTableNamesPlural2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoListWithMigrations.Migrations
+{
+    public partial class MakeTableNamesPlural2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
